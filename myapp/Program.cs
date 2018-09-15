7fa91df52b48375cfa322c6f30e9bdaab3769d8d
luqman <lpakh@hotmail.com>
2018-09-15 19:26:23 +0200

    Initial commit

---
+++ myapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myapp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("See You Again!");
+        }
+    }
+}
